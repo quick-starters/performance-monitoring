0b749a2d1398b7874865f19f6034e93572acaeff
jm-kim <jm-kim@linecorp.com>
2021-11-03 19:25:57 +0900

    Add all-in-one-test - prometheus-grafana

--- all-in-one-test/run.sh
+++ all-in-one-test/run.sh
@@ -3,6 +3,7 @@
 # Stop & Remove all running containers
 docker stop $(docker ps -a -q)
 docker rm $(docker ps -a -q)
+docker volume rm $(docker volume ls -q)
 
 # Build demo-apps
 cd demo-app
@@ -10,4 +11,8 @@ cd demo-app
 
 # Docker compose up
 cd ..
-docker compose -f ngrinder/docker-compose.yml -f demo-app/docker/docker-compose.yml up -d
+docker compose \
+  -f demo-app/docker/docker-compose.yml \
+  -f prometheus-grafana/docker-compose.yml --project-directory ./prometheus-grafana \
+  up -d
+#  -f ngrinder/docker-compose.yml \
